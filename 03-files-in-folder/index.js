bfad9e6c73989ba7ba8e686a03e50bff4176a815
Alison WEB <AlisonWEB@yandex.ru>
2024-01-22 01:40:37 +0300

    feat: solve 03 files in folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,31 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const filePath = path.join(__dirname, 'secret-folder');
+
+function getFiles() {
+  fs.readdir(filePath, (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    files.forEach((file) => {
+      fs.stat(path.join(filePath, file), (err, stats) => {
+        if (err) {
+          console.log(err);
+        } else {
+          if (!stats.isDirectory()) {
+            console.log(
+              file,
+              '-',
+              path.extname(file).slice(1),
+              '-',
+              parseFloat(stats.size / 1024).toFixed(2),
+              'kb'
+            );
+          }
+        }
+      })
+    });
+  });
+}
+getFiles();
